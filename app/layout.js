e0c37bad1ef60ab1f3880b6596d0ccbfb157e4c2
osita1234 <chukwumaositachukwuma1996@gmail.com>
2026-09-17 20:40:10 +0100

    Delete app directory

--- app/layout.js
+++
@@ -1,8 +0,0 @@
-import "./globals.css";
-export const metadata = {
-  title: "AARON WATSON | Musician • Comedian",
-  description: "Official AARON WATSON fan cards and event tickets."
-};
-export default function RootLayout({ children }) {
-  return <html lang="en"><body>{children}</body></html>;
-}
