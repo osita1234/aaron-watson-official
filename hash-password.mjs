996d9583256cb2ff87874b910213ebef0f6ca895
osita1234 <chukwumaositachukwuma1996@gmail.com>
2026-09-17 19:20:36 +0100

    Add files via upload

---
+++ hash-password.mjs
@@ -0,0 +1,7 @@
+import bcrypt from "bcryptjs";
+const password = process.argv[2];
+if (!password) {
+  console.error("Usage: node scripts/hash-password.mjs YOUR_PASSWORD");
+  process.exit(1);
+}
+console.log(await bcrypt.hash(password, 12));
